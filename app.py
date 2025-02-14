65c2818da9535bace62626dacfc6e7ba09d6bbf0
EkaMoyak <94334506+EkaMoyak@users.noreply.github.com>
2025-02-14 16:51:00 +0900

    v1

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print()
+from flask import Flask
+
+# Инициализация приложения Flask
+app = Flask(__name__)
+
+# Импорт маршрутов из routes.py
+import routes
+
+if __name__ == "__main__":
+    # Запуск приложения
+    app.run(debug=True)
